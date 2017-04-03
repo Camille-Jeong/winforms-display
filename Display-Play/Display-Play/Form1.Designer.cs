df0f9aca3ab08ea53e5e2de04557ba09edf6707b
Unknown <yangdh331@gmail.com>
2017-04-03 19:52:35 +0900

    Share source with camille

---
+++ Display-Play/Display-Play/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Display_Play
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Artist = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Artist
+            // 
+            this.Artist.AutoSize = true;
+            this.Artist.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Artist.ForeColor = System.Drawing.Color.White;
+            this.Artist.Location = new System.Drawing.Point(0, 234);
+            this.Artist.Name = "Artist";
+            this.Artist.Size = new System.Drawing.Size(1448, 135);
+            this.Artist.TabIndex = 0;
+            this.Artist.Text = "Bob Marley && The Wailers";
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(186, 439);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(945, 120);
+            this.Title.TabIndex = 1;
+            this.Title.Text = "No Woman No Cry";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1544, 809);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.Artist);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label Artist;
+        private System.Windows.Forms.Label Title;
+    }
+}
+
---
+++ Display-Play/Display-Play/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Display_Play
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Artist.Text = "i love camille so much. i will marry with her";
+            Title.Text = "i love camille so much. i will marry with her";
+            setLocation(Artist);
+            setLocation(Title);
+        }
+
+        private void setLocation(Label label)
+        {
+            int startXpos;
+            int startYpos;
+
+            if(label.Width <= this.Width)
+            {
+                startXpos = (this.Width - label.Width) / 2;
+                startYpos = label.Location.Y;
+            }
+            else
+            {
+                startXpos = 0;
+                startYpos = label.Location.Y;
+                timer.Start();
+            }
+            label.Location = new Point(startXpos, startYpos);
+        }
+
+        private void Move(Label label)
+        {
+            int xpos, ypos;
+            if(label.Right <= 0)
+            {
+                xpos = this.Width;
+                ypos = label.Location.Y;
+                label.Location = new Point(xpos, ypos);
+            }
+            else
+            {
+                xpos = label.Location.X - 1;
+                ypos = label.Location.Y;
+                label.Location = new System.Drawing.Point(xpos, ypos);
+            }
+        }
+    }
+}
